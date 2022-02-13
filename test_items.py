00024788df5fdeaf6f2579611ae04937b56aa891
nadinqa <nadin869@mail.ru>
2022-02-14 00:06:12 +0300

    test_check_button_add_to_basket function is updated

--- test_items.py
+++ test_items.py
@@ -3,12 +3,14 @@ import time
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_button_add_to_basket(browser):
+def test_check_button_add_to_basket(browser):
     browser.get(link)
     time.sleep(30)
-    button = browser.find_element_by_css_selector(".btn-add-to-basket")
-    text_add_to_basket = button.text
+    button_basket = browser.find_element_by_css_selector(".btn-add-to-basket").is_enabled()
+    #text_add_to_basket = button_basket.text
 
-    assert "Añadir al carrito" == text_add_to_basket, "Other language is selected for this page"
+    assert button_basket, "The text of element is absent on this page"
+
+    #assert button_basket == text_add_to_basket, "wrong text"
 
 #pytest --language=es test_items.py
