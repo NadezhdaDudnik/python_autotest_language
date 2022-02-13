3b595d9a136b5eb9a99e56915b4e7306fec55651
nadinqa <nadin869@mail.ru>
2022-02-13 23:17:16 +0300

    test_items.py is updated

--- test_items.py
+++ test_items.py
@@ -3,7 +3,7 @@ import time
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_guest_should_see_login_link(browser):
+def test_button_add_to_basket(browser):
     browser.get(link)
     time.sleep(30)
     button = browser.find_element_by_css_selector(".btn-add-to-basket")
