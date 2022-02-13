794108ce12c4c483d901bad96943611aae915325
nadinqa <nadin869@mail.ru>
2022-02-13 23:08:56 +0300

    3.6.7 autotest task is added

---
+++ test_items.py
@@ -0,0 +1,12 @@
+import time
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_guest_should_see_login_link(browser):
+    browser.get(link)
+    time.sleep(30)
+    button = browser.find_element_by_css_selector(".btn-add-to-basket")
+    text_add_to_basket = button.text
+
+    assert "Añadir al carrito" == text_add_to_basket, "Other language is selected"
